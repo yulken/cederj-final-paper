3559a2af90dbc921ed14059e0b05fac5d9edfa95
Caio Torres <caiocftorres@gmail.com>
2021-08-23 21:51:58 -0300

    feat: adding users routes

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,36 +1,9 @@
 import { Router } from 'express';
-
-import uploadConfig from '@config/upload';
-import multer from 'multer';
-
-import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-
-import { celebrate, Joi, Segments } from 'celebrate';
 import UsersController from '../controllers/UsersController';
-import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
-const userAvatarController = new UserAvatarController();
 
-const upload = multer(uploadConfig);
+usersRouter.post('/', usersController.create);
 
-usersRouter.post(
-  '/',
-  celebrate({
-    [Segments.BODY]: {
-      name: Joi.string().required(),
-      email: Joi.string().email().required(),
-      password: Joi.string().required(),
-    },
-  }),
-  usersController.create,
-);
-
-usersRouter.patch(
-  '/avatar',
-  ensureAuthenticated,
-  upload.single('avatar'),
-  userAvatarController.update,
-);
 export default usersRouter;
