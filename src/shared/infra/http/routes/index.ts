d8206158340bb798deb76ac27483469cd7f8fe53
Caio Torres <caiocftorres@gmail.com>
2021-08-23 21:52:18 -0300

    feat: adding global routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,15 @@
 import { Router, Request, Response } from 'express';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/health-check', (request: Request, response: Response) => {
   response.sendStatus(204);
 });
+routes.use('/users', usersRouter);
+routes.use('/profile', profileRouter);
+routes.use('/sessions', sessionsRouter);
+
 export default routes;
