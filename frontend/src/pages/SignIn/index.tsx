4cd72c8c6f93bc67019b71ff341ee43fe7c2de95
Caio Torres <caio.torres@crednovo.com.br>
2021-10-02 12:53:54 -0300

    feat(front): improving with joi

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -2,14 +2,19 @@ import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
-import * as Yup from 'yup';
 import { Link, useHistory } from 'react-router-dom';
+import Joi from 'joi';
 
 import { useAuth } from '../../hooks/auth';
-import getValidationErrors from '../../utils/getValidationErrors';
+import { useToast } from '../../hooks/toast';
+import {
+  getValidationErrors,
+  validateInput,
+} from '../../utils/getValidationErrors';
 import { Container, Content, AnimationContainer } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import ValidationError from '../../errors/ValidationError';
 
 interface SignInFormData {
   email: string;
@@ -20,33 +25,50 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const { signIn } = useAuth();
   const history = useHistory();
+  const { addToast } = useToast();
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
       try {
         formRef.current?.setErrors({});
-        const schema = Yup.object().shape({
-          email: Yup.string()
-            .required('E-mail obrigatório')
-            .email('Digite um email válido'),
-          password: Yup.string().required('Senha obrigatória'),
+        const schema = Joi.object({
+          email: Joi.string()
+            .empty()
+            .email({ tlds: { allow: false } })
+            .messages({
+              'string.empty': 'E-mail obrigatório',
+              'string.email': 'Email deve ser válido',
+            }),
+          password: Joi.string().empty().messages({
+            'string.empty': 'Senha obrigatória',
+          }),
         });
-        await schema.validate(data, {
-          abortEarly: false,
-        });
+
+        validateInput(
+          schema.validate(data, {
+            abortEarly: false,
+          }),
+        );
+
         await signIn({
           email: data.email,
           password: data.password,
         });
 
-        history.push('/');
+        history.push('/profile');
       } catch (err) {
-        if (err instanceof Yup.ValidationError) {
+        if (err instanceof ValidationError) {
           const errors = getValidationErrors(err);
           formRef.current?.setErrors(errors);
+          return;
         }
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        });
       }
     },
-    [signIn, history],
+    [signIn, addToast, history],
   );
 
   return (
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -2,11 +2,16 @@ import React, { useCallback, useRef } from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
-import * as Yup from 'yup';
+import Joi from 'joi';
 import { Link, useHistory } from 'react-router-dom';
-import getValidationErrors from '../../utils/getValidationErrors';
+import {
+  getValidationErrors,
+  validateInput,
+} from '../../utils/getValidationErrors';
+import ValidationError from '../../errors/ValidationError';
 import { Container, Content, AnimationContainer } from './styles';
 import api from '../../services/apiClient';
+import { useToast } from '../../hooks/toast';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -20,32 +25,57 @@ interface SignUpFormData {
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const history = useHistory();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(
     async (data: SignUpFormData) => {
       formRef.current?.setErrors({});
       try {
-        const schema = Yup.object().shape({
-          name: Yup.string().required('Nome obrigatório'),
-          email: Yup.string().required('E-mail obrigatório').email(),
-          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        const schema = Joi.object({
+          name: Joi.string().messages({
+            'string.empty': 'Nome obrigatório',
+          }),
+          email: Joi.string()
+            .email({ tlds: { allow: false } })
+            .messages({
+              'string.empty': 'E-mail obrigatório',
+              'string.email': 'Email deve ser válido',
+            }),
+          password: Joi.string().min(6).messages({
+            'string.min': 'No mínimo 6 dígitos',
+            'string.empty': 'Senha Obrigatória',
+          }),
+          passwordConfirmation: Joi.string()
+            .valid(Joi.ref('password'))
+            .messages({
+              'any.only': 'Senha e confirmação não correspondem',
+              'string.empty': 'Confirmação Obrigatória',
+            }),
         });
 
-        await schema.validate(data, {
-          abortEarly: false,
-        });
+        validateInput(
+          schema.validate(data, {
+            abortEarly: false,
+          }),
+        );
 
         await api.post('/users', data);
 
         history.push('/');
       } catch (err) {
-        if (err instanceof Yup.ValidationError) {
+        if (err instanceof ValidationError) {
           const errors = getValidationErrors(err);
           formRef.current?.setErrors(errors);
+          return;
         }
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        });
       }
     },
-    [history],
+    [addToast, history],
   );
 
   return (
