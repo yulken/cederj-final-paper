cd2ee0802cb3f30c76c8dcdc4b629ac85d2167d9
Caio Torres <caiocftorres@gmail.com>
2021-10-06 21:51:57 -0300

    feat(backend): adding port and address to config

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -5,28 +5,31 @@ import cors from 'cors';
 import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
 import '@shared/container';
+import log from '@shared/utils/log';
 import routes from './routes';
 import 'reflect-metadata';
-import log from '@shared/utils/log';
 // todo configure production environment
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
-  log.error(err);
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      message: err.message,
-    });
-  }
-  
-  return response.status(500).json({
-    message: 'Internal server error',
-  });
-});
+app.use(
+  (err: Error, _request: Request, response: Response, _next: NextFunction) => {
+    log.error(err);
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
 
-const port = 3333;
-app.listen(port, '127.0.0.1', () => {
-  log.info(`Server started on port ${port}!`);
+    return response.status(500).json({
+      message: 'Internal server error',
+    });
+  },
+);
+
+const port = Number(process.env.APP_API_PORT) || 3333;
+const address = process.env.APP_API_ADDRESS || 'localhost';
+app.listen(port, address, () => {
+  log.info(`Server started => ${address}:${port}!`);
 });
